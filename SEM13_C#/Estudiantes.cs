2d9c0e41c6aa6ce1fee4884cce07e60d6acff37f
7anvz <danjscommunity@gmail.com>
2025-11-11 12:16:30 -0500

    Sem13

---
+++ SEM13_C#/Estudiantes.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM13_C_
+{
+    internal class Estudiantes
+    {
+        string[] nombres = new string[0];
+        byte[] edades = new byte[0];
+        int pos = 0;
+
+        public void insertar()
+        {
+            string seguir;
+            while (true)
+            {
+                Array.Resize(ref nombres, nombres.Length + 1);
+                Array.Resize(ref edades, edades.Length + 1);
+
+                Console.Write("\nIngrese nombre: ");
+                nombres[pos] = Console.ReadLine();
+                Console.Write("Ingrese edad: ");
+                edades[pos] = byte.Parse(Console.ReadLine());
+                pos++;
+
+                Console.WriteLine("\nSe registró correctamente");
+
+                while (true)
+                {
+                    Console.Write("\n¿Desea seguir registrando? (S/N): ");
+                    seguir = Console.ReadLine();
+                    if (seguir == "s" || seguir == "n") break;
+                    else Console.WriteLine("Opción no válida\n");
+                }
+                if (seguir == "n") break ;
+            }
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("POS\tNOMBRE\t\tEDAD");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine($"{i}\t{nombres[i]}\t\t{edades[i]}");
+            }
+        }
+
+        public int buscar(string nom)
+        {
+            Console.Write("Ingrese el nombre  buscar: ");
+            string buscar= Console.ReadLine();
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                if (nom == nombres[i])
+                    return i;
+            }
+            return -1;
+        }
+
+        public void modificar() 
+        {
+            Console.Write("\nIngrese el nombre a modificar: ");
+            string nom = Console.ReadLine();
+
+            int indice = buscar(nom);
+
+            if (indice != 1)
+            {
+                Console.Write("\nIngrese el nuevo nombre: ");
+                nombres[indice] = Console.ReadLine();
+                Console.Write("Ingrese la nueva edad: ");
+                edades[indice] = byte.Parse(Console.ReadLine());
+            }
+            else Console.WriteLine("No existe");
+        }
+
+        public void eliminar()
+        {
+            Console.Write("\nIngrese el nombre a eliminar: ");
+            string nom = Console.ReadLine();
+
+            int indice = buscar(nom);
+
+            if (indice != 1)
+            {
+                for (int i = 0; i < nombres.Length - 1; i++) 
+                {
+                    nombres[i] = nombres[i + 1];
+                    edades[i] = edades[i + 1];
+                }
+
+                Array.Resize(ref nombres, nombres.Length - 1);
+                Array.Resize(ref edades, edades.Length - 1);
+                pos--;
+            }
+            else Console.WriteLine("No existe");
+        }
+    }
+}
---
+++ SEM13_C#/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM13_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string men;
+            Estudiantes e = new Estudiantes();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("BIENVENIDOS AL SISTEMA DE REGISTRO DE EDADES\n");
+                Console.WriteLine("********** Menú de opciones*********");
+                Console.WriteLine("*   1. Insertar                    *");
+                Console.WriteLine("*   2. Mostrar                     *");
+                Console.WriteLine("*   3. Bucar                       *");
+                Console.WriteLine("*   4. Modificar                   *");
+                Console.WriteLine("*   5. Eliminar                    *");
+                Console.WriteLine("*   6. Ordenar                     *");
+                Console.WriteLine("*   0. Salir                       *");
+                Console.WriteLine("************************************\n");
+
+                int opc;
+
+                while (true)
+                {
+                    Console.Write("Ingrese una opción: ");
+                    if (int.TryParse(Console.ReadLine(), out opc) && opc >= 0 && opc <= 6) break;
+                    else Console.WriteLine("Ingrese una opción correcta!\n");
+                }
+
+                switch (opc)
+                {
+                    case 0: return;
+                    case 1: e.insertar(); break;
+                    case 2: e.mostrar(); break;
+                    case 3:
+                        e.mostrar();
+                        Console.WriteLine("Ingrese el nombre a buscar: ");
+                        string buscar = Console.ReadLine();
+                        int indice = e.buscar(buscar);
+                        if (indice != 1)
+                            Console.WriteLine($"\n{buscar} está en la posición {indice}");
+                        else Console.WriteLine($"\n{buscar} no existe");
+                            break;
+                    case 4: 
+                        e.mostrar();
+                        e.modificar();
+                        e.mostrar();
+                        break;
+                    case 5:
+                        e.mostrar();
+                        e.eliminar();
+                        e.mostrar();
+                        break;
+                    case 6: break;
+                }
+
+                while (true)
+                {
+                    Console.Write("\n¿Deseas regresar al menú? (S/N): ");
+                    men = Console.ReadLine();
+                    if (men == "s" || men == "n") break;
+                    else Console.WriteLine("Ingrese una opción correcta!\n");
+                }
+            } while (men == "s");
+        }
+    }
+}
